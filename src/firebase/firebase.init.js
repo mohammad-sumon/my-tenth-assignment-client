2c4cae3f775c1ab2beb528f841d41f3c64959df5
Mohammad Sumon <sumon3011@gmail.com>
2022-10-26 14:50:09 +0600

    environment variable added

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,15 +5,27 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCo-y4cXmXYeRdEc8k0zKIAttUE4S3kM08",
-  authDomain: "expert-programmer-client.firebaseapp.com",
-  projectId: "expert-programmer-client",
-  storageBucket: "expert-programmer-client.appspot.com",
-  messagingSenderId: "496935028096",
-  appId: "1:496935028096:web:e4b6e25411c22d6424ad65"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
+
+/*
+apiKey: "AIzaSyCo-y4cXmXYeRdEc8k0zKIAttUE4S3kM08",
+  authDomain: "expert-programmer-client.firebaseapp.com",
+  projectId: "expert-programmer-client",
+  storageBucket: "expert-programmer-client.appspot.com",
+  messagingSenderId: "496935028096",
+  appId: "1:496935028096:web:e4b6e25411c22d6424ad65"
+
+
+*/
--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -4,57 +4,72 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import Logo from '../../../assets/logo.png';
+import Logo from "../../../assets/logo.png";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
-import './Header.css';
+import "./Header.css";
+import { FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
-    const {user, logOut, setUser} = useContext(AuthContext);
+  const { user, logOut, setUser } = useContext(AuthContext);
 
-    const handleLogOut = () => {
-        logOut()
-        .then(() => {
-            setUser({});
-        })
-        .catch((error) => {
-            setUser({});
-          console.error(error);
-        })
-      }
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+        console.error(error);
+      });
+  };
 
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
           <Navbar.Brand className="website-name">
-            <Link  to='/'>
-            <img src={Logo} alt="" className="logo"/>
-            Expert Programmer
+            <Link to="/">
+              <img src={Logo} alt="" className="logo" />
+              Expert Programmer
             </Link>
-          
-            </Navbar.Brand>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#features"><Link to='/'>Home</Link></Nav.Link>
-              <Nav.Link href="#pricing"><Link to='/courses'>Courses</Link></Nav.Link>
-              <Nav.Link href="#pricing"><Link to='/blog'>Blog</Link></Nav.Link>
-              <Nav.Link href="#pricing"><Link to='/faq'>FAQ</Link></Nav.Link>
-              <Nav.Link href="#pricing"><Link to='/login'>Login</Link></Nav.Link>
-              
+              <Nav.Link href="#features">
+                <Link to="/">Home</Link>
+              </Nav.Link>
+              <Nav.Link href="#pricing">
+                <Link to="/courses">Courses</Link>
+              </Nav.Link>
+              <Nav.Link href="#pricing">
+                <Link to="/blog">Blog</Link>
+              </Nav.Link>
+              <Nav.Link href="#pricing">
+                <Link to="/faq">FAQ</Link>
+              </Nav.Link>
+              <Nav.Link href="#pricing">
+                <Link to="/login">Login</Link>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
           <nav>
-          <>
+         
+            <>
               {user?.uid && (
                 <>
-                  <img src={user.photoURL} alt="" className="rounded-circle" style={{width: '30px'}} title={user.displayName}/>
+                  <img
+                    src={user.photoURL}
+                    alt=""
+                    className="rounded-circle"
+                    style={{ width: "30px" }}
+                    title={user.displayName}
+                  />
                   <Button variant="light" onClick={handleLogOut}>
                     Log Out
                   </Button>
                 </>
-              )} 
-              
+              )}
             </>
             <button className="btn btn-secondary">Dark</button>
           </nav>
